e9bc383942f45417c1145a4c369adb835719bfc9
OSPK <waqas@opensource.com.pk>
2013-10-21 00:12:09 +0500

    modalities

--- templates-pages/ospk-home.php
+++ templates-pages/ospk-home.php
@@ -47,63 +47,80 @@ get_header(); ?>
 
     <div class="sect-border"></div>
 
-    <div class="container">
-
-      <h3 id="tour" class="subhead">How does it work?</h3>
-      <div class="row">
-        <div class="col-md-12">
-          <div id="slideshow" class="carousel slide">
-            <ol class="carousel-indicators">
-              <li data-target="#slideshow" data-slide-to="0" class="active"></li>
-              <li data-target="#slideshow" data-slide-to="1"></li>
-              <li data-target="#slideshow" data-slide-to="2"></li>
-              <li data-target="#slideshow" data-slide-to="3"></li>
-              <li data-target="#slideshow" data-slide-to="4"></li>
-            </ol>
-
-            <div class="carousel-inner">
-              <div class="item active">
-                <img src="<?php bloginfo('template_directory'); ?>/assets/img/stock1.jpg">
-                <div class="carousel-caption">
-                  Amazing photos provided by
-                </div>
-              </div>
-              <div class="item">
-                <img src="<?php bloginfo('template_directory'); ?>/assets/img/stock2.jpg">
-                <div class="carousel-caption">
-                  Boat in Water
-                </div>
-              </div>
-              <div class="item">
-                <img src="<?php bloginfo('template_directory'); ?>/assets/img/stock3.jpg">
-                <div class="carousel-caption">
-                  Millennium Bridge in London
-                </div>
-              </div>
-              <div class="item">
-                <img src="<?php bloginfo('template_directory'); ?>/assets/img/stock4.jpg">
-                <div class="carousel-caption">
-                  Blurs
-                </div>
-              </div>
-              <div class="item">
-                <img src="<?php bloginfo('template_directory'); ?>/assets/img/stock5.jpg">
-                <div class="carousel-caption">
-                  Santorini Greece
-                </div>
-              </div>
+            <!-- Button trigger modal -->
+          <div class="container">
+            <div class="modality">
+            <a data-toggle="modal" href="#myModal" class="btn btn-primary btn-lg">How does it work?</a>
             </div>
+          </div>
 
-            <a class="left carousel-control" href="#slideshow" data-slide="prev">
-              <span class="glyphicon"><i class="icon-arrow-left"></i></span>
-            </a>
-            <a class="right carousel-control" href="#slideshow" data-slide="next">
-              <i class="icon-arrow-right"></i>
-            </a>
-          </div> <!-- #slideshow -->
-        </div> <!-- /.col-md-12 -->
-      </div> <!-- /.row -->
-      </div>
+              <!-- Modal -->
+          <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                  <h4 class="modal-title">How does it work?</h4>
+                </div>
+                <div class="modal-body">
+
+
+                  <div id="slideshow" class="carousel slide">
+                    <ol class="carousel-indicators">
+                      <li data-target="#slideshow" data-slide-to="0" class="active"></li>
+                      <li data-target="#slideshow" data-slide-to="1"></li>
+                      <li data-target="#slideshow" data-slide-to="2"></li>
+                      <li data-target="#slideshow" data-slide-to="3"></li>
+                      <li data-target="#slideshow" data-slide-to="4"></li>
+                    </ol>
+
+                    <div class="carousel-inner">
+                      <div class="item active">
+                        <img src="<?php bloginfo('template_directory'); ?>/assets/img/stock1.jpg">
+                        <div class="carousel-caption">
+                          Amazing photos provided by
+                        </div>
+                      </div>
+                      <div class="item">
+                        <img src="<?php bloginfo('template_directory'); ?>/assets/img/stock2.jpg">
+                        <div class="carousel-caption">
+                          Boat in Water
+                        </div>
+                      </div>
+                      <div class="item">
+                        <img src="<?php bloginfo('template_directory'); ?>/assets/img/stock3.jpg">
+                        <div class="carousel-caption">
+                          Millennium Bridge in London
+                        </div>
+                      </div>
+                      <div class="item">
+                        <img src="<?php bloginfo('template_directory'); ?>/assets/img/stock4.jpg">
+                        <div class="carousel-caption">
+                          Blurs
+                        </div>
+                      </div>
+                      <div class="item">
+                        <img src="<?php bloginfo('template_directory'); ?>/assets/img/stock5.jpg">
+                        <div class="carousel-caption">
+                          Santorini Greece
+                        </div>
+                      </div>
+                    </div>
+
+                    <a class="left carousel-control" href="#slideshow" data-slide="prev">
+                      <span class="glyphicon"><i class="icon-arrow-left"></i></span>
+                    </a>
+                    <a class="right carousel-control" href="#slideshow" data-slide="next">
+                      <i class="icon-arrow-right"></i>
+                    </a>
+                  </div> <!-- #slideshow -->
+
+
+              </div><!--/.modalbody-->
+
+              </div><!-- /.modal-content -->
+            </div><!-- /.modal-dialog -->
+          </div><!-- /.modal -->
 
       <div class="sect-border"></div>
 
